1dbdae3761e17b6b8da0042986a07cb31d3dbcc4
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-11-05 11:02:35 +0100

    authentication added

--- test/app/api/[...nextauth]/options.js
+++ test/app/api/[...nextauth]/options.js
@@ -19,11 +19,47 @@ export const Options = {
           role: userRole,
         };
       },
+
+    
+     clientId:process.env.GITHUB_ID , 
+     clientSecret:process.env.GITHUB_SECRET_ID 
+
+
     }),
 
-    GoogleProvider({}),
+
+GoogleProvider({
+     
+profile(profile){
+    console.log("Profile Google : " , profile )
+
+
+return {
+    ...profile , 
+    id:profile.sub , 
+    role:userRole ,
+}
+
+
+}
+,
+     clientId:process.env.GOOGLE_ID , 
+     clientSecret:process.env.GOOGLE_SECRET_ID 
+    }),
   ],
-  callbacks: {},
+  callbacks: {
+    async jwt({token , user }){
+
+        if (user) token.role = user.role
+        return token ; 
+    }, 
+
+    async session ({session , token }){
+        if (session?.user) session.user.role = token.role ; 
+
+        return session ;  
+    }
+  },
 
   pages: {},
   sessions: {},
